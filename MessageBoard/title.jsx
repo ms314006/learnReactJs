cfc686ba9fb0336099c9eab393ba60e69b2ff8d1
GQSM <ms314006@hotmail.com>
2018-10-04 02:32:57 +0800

    update

--- MessageBoard/title.jsx
+++ MessageBoard/title.jsx
@@ -69,10 +69,10 @@ class MessageForm extends React.Component{
 }
 
 //訊息資料
-let data = [{name:'神Ｑ',message:'嗨！大家好啊！'},
+let data = [{name:'神Q',message:'嗨！大家好啊！'},
             {name:'小馬',message:'早安啊！昨天有沒有好好發文？'},
             {name:'王子',message:'ㄛ！別說了，那真的超級累！'},
-            {name:'神Ｑ',message:'哈哈哈！加油啦！再一下就結束了！'},
+            {name:'神Q',message:'哈哈哈！加油啦！再一下就結束了！'},
             {name:'王子',message:'結束後我一定要爆睡一頓！'},]
 
 ReactDOM.render(<MessageForm  messageData={data} />, document.getElementById('root'))
