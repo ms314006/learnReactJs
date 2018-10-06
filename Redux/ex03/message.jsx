c7829837693a73d7efafd7393f1a5615bbe43196
GQSM <ms314006@hotmail.com>
2018-10-07 00:28:23 +0800

    ex03

---
+++ Redux/ex03/message.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
+import {connect} from "react-redux"
+import store from "./index.js"
+
+class MessageList extends React.Component {
+    render(){
+        let message = this.props.data.map((item)=>{
+            return <li>{item.name}：{item.message}</li>
+        })
+        return(
+            <ul>
+                {message}
+            </ul>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return { data: state.message }       
+}
+
+const List = connect(mapStateToProps)(MessageList)
+
+class MessageForm extends React.Component {
+    render(){
+        return(
+            <Provider store={store}>
+                <List />
+            </Provider>
+        )
+    }
+}
+
+ReactDOM.render(<MessageForm/>,document.getElementById('root'))
