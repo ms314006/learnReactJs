120492b8a01b00360e892175d7b688af99a0ce1a
GQSM <ms314006@hotmail.com>
2018-10-10 21:13:01 +0800

    ex03

---
+++ ReactRouter/ex03/src/components/About/About.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+class About extends React.Component{
+    render(){
+        return <p>這邊是關於我們</p>
+    }
+}
+
+export {About}
---
+++ ReactRouter/ex03/src/components/Main/Main.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { BrowserRouter, Route, HashRouter, Link } from "react-router-dom"
+import { Title } from "../Title"
+import { Home } from "../Home"
+import { About } from "../About"
+
+class Main extends React.Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <Title title="功能選單" />
+                    <ul>
+                        <li><Link to="/">回到首頁</Link></li>
+                        <li><Link to="/about">關於我們</Link></li>
+                    </ul>
+                    <hr />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                </div>
+            </HashRouter>
+        )
+    }
+}
+
+export { Main }
