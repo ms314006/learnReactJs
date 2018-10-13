1f11d9974ca014070870004ea7a6917bc50e8f88
GQSM <ms314006@hotmail.com>
2018-10-13 16:48:12 +0800

    ex06

---
+++ ReactRouter/ex06/src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+class Home extends React.Component{
+    render(){
+        return <p>這裡是首頁</p>
+    }
+}
+
+export {Home}
---
+++ ReactRouter/ex06/src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+class Title extends React.Component {
+    render(){
+        return <h1>{this.props.title}</h1>
+    }
+}
+
+export {Title}
