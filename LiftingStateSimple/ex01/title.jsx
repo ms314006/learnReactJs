c003e688c44737c3df4d96b255fd6cf024d5e2cb
GQSM <ms314006@hotmail.com>
2018-10-15 19:08:17 +0800

    mod comm

--- LiftingStateSimple/ex01/title.jsx
+++ LiftingStateSimple/ex01/title.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 //判斷溫度是否達沸點
 class Title extends React.Component {
     render(){
-        //溫度100度以上就到達沸點
+        //判斷溫度100度以上就到達沸點
         return <h1>{(this.props.temperature>=100 ? '達到沸點!!!':'未到沸點...')}</h1>
     }
 }
@@ -21,7 +21,7 @@ class Celsius extends React.Component {
     changeState(event){
         //取得目前輸入的值
         let temperature = event.target.value
-        //把值寫到state裡面，並判斷是否大於100超過就更改成達到沸點，否則就未到沸點...
+        //把值寫到state裡面
         this.setState({ [event.target.name] : temperature })
     }
 
